7940eea0f9b4dd904688c2b86ff8980634c641ca
eamonge <39276293+eamonge@users.noreply.github.com>
2022-03-10 13:40:28 -0600

    sadfsdf
    sdfsdf

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Sample Widget</p>
+      <script async id="load_js" src="https://ticnoovalvc.audara.cloud/apps/livechat/apps/chat/External/js/load.js?widget=1"></script>
+    </div>
+  );
+}
+
+export default App;
